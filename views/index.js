728515cf4164b91c0b9c526268d1e30c217f4948
Arthursouto099 <arthurtavaressouto@gmail.com>
2024-12-04 23:09:18 -0300

    update views

--- views/index.js
+++ views/index.js
@@ -1,12 +1,80 @@
+
+
+
 const urlGet = "http://localhost:3001/habitos"
 const urlGetByID = "http://localhost:3001/habitos/"
 const urlPost = "http://localhost:3001/habitos"
 const urlPut = `http://localhost:3001/habitos/`
+const urlDelete = 'http://localhost:3001/habitos/'
 
 
-fetch(urlGet)
-    .then(response => { return response.json() })
-    .then(data => display(data))
+function exibirTodosHabitos(funcao) {
+    fetch(urlGet)
+        .then(response => response.json())
+        .then(data => funcao(data))
+}
+
+exibirTodosHabitos(display)
+
+
+function listaDeIds(data) {
+   console.log(data)
+
+}
+
+// async function retornaData() {
+//     try {
+//         const response = await fetch(urlGet)
+//         const data = await response.json()
+//         return data
+//     }catch(error) {
+//         console.error(error)
+//         throw error
+//     }
+// }
+
+async function retornaData() {
+    try {
+        const response = await fetch(urlGet)
+        const data = await response.json()
+        return data
+        
+    }catch(error) {
+        console.error(error)
+        throw error
+    }
+}
+// teste 1
+// console.log(retornaData().then((response) => console.log(response)))
+
+async function verificarId(id) {
+    const habitos = await retornaData()
+
+    let verificacao = false
+
+    for (const habito of habitos) {
+        if (habito.id === id) {
+            console.log('ID encontrado');
+            verificacao = true;
+            break; // Interrompe o loop assim que o ID é encontrado
+        }
+    }
+
+    return verificacao
+   
+}
+
+
+verificarId(5)
+.then(resultado => {
+    if(resultado === false) {
+        console.log('oii')
+    }
+})
+
+// if(verificarId(1) === true) {
+//     console.log('oii')
+// }
 
 
 function display(data) {
@@ -39,27 +107,27 @@ function submit() {
     const input3 = document.getElementById("input-id")
 
     if (input1.value === '' | input2.value === '' | input3.value === '') {
-        alert("Os dados não podem ser vazios")
+        return alert("Os dados não podem ser vazios")
     }
 
 
 
-    else {
-        fetch(urlGet, {
-            method: "POST",
-            headers: { 'Content-Type': "application/json" },
-            body: JSON.stringify({ Nome_Habito: input1.value, Descricao_Do_Habito: input2.value, Identidade_Habito: input3.value })
-        })
-            .then(response => { return response.json() })
-            .then(data => console.log(data))
 
-        alert("Caso seu hábito não tenha aparecido, por favor atualize a página.")
+    fetch(urlGet, {
+        method: "POST",
+        headers: { 'Content-Type': "application/json" },
+        body: JSON.stringify({ Nome_Habito: input1.value, Descricao_Do_Habito: input2.value, Identidade_Habito: input3.value })
+    })
+        .then(response => { return response.json() })
+        .then(data => console.log(data))
 
-        createArea.style.display = "none"
-        habitosArea.style.display = "flex"
+    alert("Caso seu hábito não tenha aparecido, por favor atualize a página.")
+
+    createArea.style.display = "none"
+    habitosArea.style.display = "flex"
 
 
-    }
+
 
 
 
@@ -68,32 +136,38 @@ function submit() {
 
 
 function exibirTelaEdit() {
+    const id = prompt("Digite o ID do hábito que deseja editar!")
+
+    verificarId(parseInt(id)).then(resultado => {
+        if(resultado === false) {
+            alert("O ID INFORMADO NÃO EXISTE")
+            return new Error("ID NÂO ENCONTRADO")
+        }
+    })
+
     const editArea = document.getElementById("editar-area")
     habitosArea.style.display = 'none'
     editArea.style.display = 'flex'
 
-    const id = prompt("Digite o ID do hábito que deseja editar!")
-
-    
     const result = document.getElementById("result_id")
     result.innerHTML = ''
 
 
-   const el = (data) => { 
-    data.forEach(d => {
-        const habitoDiv = document.createElement("div")
-        habitoDiv.innerHTML = `<p><STRONG>ID:</strong> ${d.id} <br> <strong>NOME DO HÁBITO:</strong> ${d.Nome_Habito} <br> <strong>DESCRIÇÃO:</strong> ${d.Descricao_Do_Habito}, <br> <strong>IDENTIDADE:</strong> ${d.Identidade_Habito}</p>`
-        habitoDiv.className = "habitos_results"
-        const button = document.createElement("button")
-        button.className = "button-delete"
-        result.appendChild(habitoDiv)
+    const el = (data) => {
+        data.forEach(d => {
+            const habitoDiv = document.createElement("div")
+            habitoDiv.innerHTML = `<p><STRONG>ID:</strong> ${d.id} <br> <strong>NOME DO HÁBITO:</strong> ${d.Nome_Habito} <br> <strong>DESCRIÇÃO:</strong> ${d.Descricao_Do_Habito}, <br> <strong>IDENTIDADE:</strong> ${d.Identidade_Habito}</p>`
+            habitoDiv.className = "habitos_results"
+            const button = document.createElement("button")
+            button.className = "button-delete"
+            result.appendChild(habitoDiv)
 
-    });
-   }
+        });
+    }
 
-   fetch(urlGetByID + id)
-   .then(response => { return response.json() })
-   .then(data => el(data))
+    fetch(urlGetByID + id)
+        .then(response => { return response.json() })
+        .then(data => el(data))
 }
 
 
@@ -102,31 +176,59 @@ function exibirHabitoEditar() {
     const input2 = document.getElementById("input-desc-2")
     const input3 = document.getElementById("input-id-3")
 
-    
 
-  const dadosAtualizados = {
-    Nome_Habito: input1.value,
-    Descricao_Do_Habito: input2.value,
-    Identidade_Habito: input3.value
-  }
 
-  const id = prompt("Digite o ID do hábito que deseja editar novamente, o id deve ser o mesmo que o anterior!")
+    const dadosAtualizados = {
+        Nome_Habito: input1.value,
+        Descricao_Do_Habito: input2.value,
+        Identidade_Habito: input3.value
+    }
 
-  fetch(urlPut + id, {
-    method: "PUT",
-    headers: {'Content-Type': "application/json"},
-    body: JSON.stringify(dadosAtualizados)
+    const id = prompt("Digite o ID do hábito que deseja editar novamente, o id deve ser o mesmo que o hábito abaixo!: ")
 
-    
-    
-  }) 
+    verificarId(parseInt(id)).then(resultado => {
+        if(resultado === false) {
+            alert("O ID INFORMADO NÃO É O MESMO!")
+            return new Error("ID NÂO ENCONTRADO")
+        }
+    })
 
-  const editArea = document.getElementById("editar-area")
-  alert("Caso seu hábito não tenha aparecido, por favor atualize a página.")
+    fetch(urlPut + id, {
+        method: "PUT",
+        headers: { 'Content-Type': "application/json" },
+        body: JSON.stringify(dadosAtualizados)
 
-  editArea.style.display = "none"
-  habitosArea.style.display = "flex"
 
-   
+
+    })
+
+    const editArea = document.getElementById("editar-area")
+    alert("Caso seu hábito não tenha aparecido, por favor atualize a página.")
+
+    editArea.style.display = "none"
+    habitosArea.style.display = "flex"
+
+
 }
+
+function deletarHabito() {
+    const id = prompt("Digite o ID do hábito: ")
+
+    verificarId(parseInt(id)).then(resultado => {
+        if(resultado === false) {
+            alert("O ID INFORMADO NÃO EXISTE")
+            return new Error("ID NÂO ENCONTRADO")
+
+        }
+    })
+    
+
+
+    fetch(urlDelete + id, {
+        method: "DELETE",
+        headers: { 'Content-Type': "application/json" },
+    })
+}
+
+
 
